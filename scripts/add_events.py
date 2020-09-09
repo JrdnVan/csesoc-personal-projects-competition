96811e5d5b658e850383582c004381e12bfd459a
Neil50 <neil@DESKTOP-K9GVNNA.localdomain>
2020-09-09 14:03:40 +1000

    Need to add verification checks and generate UID

---
+++ scripts/add_events.py
@@ -0,0 +1,63 @@
+import boto3
+import datetime
+
+# Call user/event table from AWS
+
+dynamodb = boto3.client ("dynamodb")
+
+
+#Note: generate Randon ID
+# add_event_to_table("neil", "1001", "location", "Desc", "phy", "5" , "6",  "7" )
+
+
+
+
+# If creating new event attributes cant be empty
+# if person_limit, time_limit and radius are empty enter default values
+
+
+
+
+def add_event_to_table (name, host_id, location , description, photo , person_limit, time_limit, radius):
+    
+    if type(name) == str and type(location) and str and type(host_id) and str:
+        if name == "" or location =="" or host_id == "":
+            print("User not added")
+            return False
+            
+            
+    try:
+        # Create transaction
+        resp = dynamodb.transact_write_items(
+            TransactItems=[
+                {
+                    "Put": {
+                        "TableName": "meet_ball_user",
+                        "Item": {
+
+                            "UID_User": {"S": host_id},
+                            "UID_Event/User": {"S": "1089"},
+                            "Name" : {"S": name},
+                            "location": {"S": location},
+                            "description" : {"S": description},
+                            "photo": {"S": photo},
+                            "person_limit" : {"N" : person_limit},
+                            "time_limit": {"N": time_limit},
+                            "radius": {"N": radius},
+                            # Attain time event it made
+                            "time_stamp" : {"S" : datetime.datetime.now().isoformat()},
+                            
+                        },
+                        "ConditionExpression": "attribute_not_exists(UID_User)",
+                        "ReturnValuesOnConditionCheckFailure": "ALL_OLD",
+                    }
+                }
+            ]
+        )
+        print("Event is now added")
+        return True
+    except Exception as e:
+        print("Could not add event to database")
+        
+
+add_event_to_table("neil", "1009", "location", "ff", "phy", "0" , "6",  "7" )
