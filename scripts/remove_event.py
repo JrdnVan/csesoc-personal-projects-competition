579140c24b620b3dc77f172839152849bb4351fb
Neil50 <neil@DESKTOP-K9GVNNA.localdomain>
2020-09-10 12:54:37 +1000

    Need to add conditional delete

--- scripts/remove_event.py
+++ scripts/remove_event.py
@@ -1,15 +1,20 @@
 import boto3
 from boto3.dynamodb.conditions import Key
+from decouple import Config, RepositoryEnv
 
-# boto3 is the AWS SDK library for Python.
-# The "resources" interface allows for a higher-level abstraction than the low-level client interface.
-# For more details, go to http://boto3.readthedocs.io/en/latest/guide/resources.html
-dynamodb = boto3.resource('dynamodb')
+
+
+DOTENV_PATH = ".env"
+env = Config(RepositoryEnv(DOTENV_PATH))
+
+# Call user/event table from AWS
+session = boto3.Session(
+    aws_access_key_id=env.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=env.get('AWS_SECRET_ACCESS_KEY'),
+)
+s3 = session.client('s3')
+dynamodb = session.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table('meet_ball_user')
-
-
-#delete_event("1009", "1089")
-
 
 
 # Requires Host and Event_id 
@@ -39,4 +44,4 @@ def delete_event (host_id, event_id):
         
         
     
-    
+delete_event("123123123", "urn:uui:3108698e-e4dc-444f-b6d5-a47d61341d60")
